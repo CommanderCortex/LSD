e199577e5b66b7c1b695e91cdf63dcb8cb5b231e
CommanderCortex <cjcotton88@gmail.com>
2019-06-20 13:11:38 +1200

    Added Block Types, Started Parser

---
+++ LSD/src/me/cortex/LSD/Parser/Parser.java
@@ -0,0 +1,12 @@
+package me.cortex.LSD.Parser;
+
+import me.cortex.LSD.Block;
+import me.cortex.LSD.Tokenizer.Tokenizer;
+
+public abstract class Parser<T extends Block> {
+	
+	public abstract boolean shouldParse(String line);
+	
+	public abstract T parse(Block superBlock, Tokenizer tokenizer);
+
+}
